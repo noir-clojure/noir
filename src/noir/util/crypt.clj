bdeb37ad46d8e00ba9d209a76d7b51159d5ac5ef
Pierre-Yves Ritschard <pyr@milestonelab.com>
2011-06-22 11:52:21 +0200

    Replace util/crypt's SHA based implementation by a bcrypt based one which provides much greater security.
    See http://codahale.com/how-to-safely-store-a-password/ for details.

--- src/noir/util/crypt.clj
+++ src/noir/util/crypt.clj
@@ -1,34 +1,21 @@
 (ns noir.util.crypt
   "Simple functions for encrypting strings and comparing them. Typically used for storing passwords."
   (:refer-clojure :exclude [compare])
-  (:require [ring.util.codec :as codec])
-  (:import java.security.MessageDigest java.security.SecureRandom))
+  (:import [org.mindrot.jbcrypt BCrypt]))
 
-(defn gen-salt []
-  (let [salt (byte-array 8)]
-    ;; generate a truly random sequence of bytes
-    (.. SecureRandom (getInstance "SHA1PRNG") (nextBytes salt))
-    salt))
-
-(defn ext-salt [stored]
-  (byte-array (take 8 (codec/base64-decode stored))))
+(defn gen-salt
+  ([size]
+   (BCrypt/gensalt size))
+  ([]
+   (BCrypt/gensalt)))
 
 (defn encrypt 
   "Encrypt the given string with a generated or supplied salt. Uses SHA-256 encryption."
   ;; generate a salt
-  ([raw] (encrypt (gen-salt) raw))
-  ([salt raw]
-    ;; append the salt to the front
-   (str (codec/base64-encode salt) 
-        (codec/base64-encode (let [mdig (. MessageDigest getInstance "SHA-256")
-                                   raw-bytes (codec/base64-decode raw)]
-                               (. mdig reset)
-                               ;; use the salt to encrypt
-                               (. mdig update salt)
-                               (. mdig digest raw-bytes))))))
+  ([salt raw] (BCrypt/hashpw raw salt))
+  ([raw] (encrypt (gen-salt) raw)))
 
-(defn compare 
+(defn compare
   "Compare a raw string with an already encrypted string"
   [raw encrypted]
-  (= (encrypt (ext-salt encrypted) raw) encrypted))
-
+  (BCrypt/checkpw raw encrypted))
