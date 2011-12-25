4be96ce28c4f3583c859dbaf4a784de6cde4643d
Anthony Grimes <i@raynes.me>
2011-12-25 15:55:44 -0600

    Fix indentation in project.clj.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject noir "1.3.0-alpha2"
-            :description "Noir - a clojure web framework"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [compojure "1.0.0"]
-                           [org.clojure/tools.namespace "0.1.0"]
-                           [cheshire "2.0.4"]
-                           [ring "1.0.1"]
-                           [hiccup "0.3.7"]
-                           [clj-stacktrace "0.2.3"]
-                           [ring-reload-modified "0.1.1"]
-                           [net.java.dev.jets3t/jets3t "0.8.1"]
-                           [org.mindrot/jbcrypt "0.3m"]])
+  :description "Noir - a clojure web framework"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "1.0.0"]
+                 [org.clojure/tools.namespace "0.1.0"]
+                 [cheshire "2.0.4"]
+                 [ring "1.0.1"]
+                 [hiccup "0.3.7"]
+                 [clj-stacktrace "0.2.3"]
+                 [ring-reload-modified "0.1.1"]
+                 [net.java.dev.jets3t/jets3t "0.8.1"]
+                 [org.mindrot/jbcrypt "0.3m"]])
