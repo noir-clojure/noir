67da279eb415875850ceca5eace6fabccb180b3a
Chris Granger <ibdknox@gmail.com>
2011-12-25 14:21:40 -0500

    inc to 1.2.2 with compojure 1.0.0
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject noir "1.2.2-SNAPSHOT"
+(defproject noir "1.2.2"
             :description "Noir - a clojure web framework"
             :dependencies [[org.clojure/clojure "[1.2.1],[1.3.0]"]
-                           [compojure "1.0.0-RC2"]
+                           [compojure "1.0.0"]
                            [org.clojure/tools.namespace "0.1.0"]
                            [clj-json "0.4.3"]
                            [ring "1.0.1"]
