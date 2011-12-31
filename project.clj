2251c79c3683181d2d6e6a89e504d0f65d81d723
Anthony Grimes <i@raynes.me>
2011-12-31 11:57:05 -0600

    Inc alpha.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir "1.3.0-alpha2"
+(defproject noir "1.3.0-alpha3"
   :description "Noir - a clojure web framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.0"]
