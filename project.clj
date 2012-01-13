4ac12ae15692d3a7e595cae1ba2d638d9d6372cc
Chris Granger <ibdknox@gmail.com>
2012-01-13 10:40:14 -0800

    move to latest clj-stacktrace, inc alpha4
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir "1.3.0-alpha3"
+(defproject noir "1.3.0-alpha4"
   :description "Noir - a clojure web framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.0"]
@@ -6,7 +6,7 @@
                  [cheshire "2.0.4"]
                  [ring "1.0.1"]
                  [hiccup "0.3.7"]
-                 [clj-stacktrace "0.2.3"]
+                 [clj-stacktrace "0.2.4"]
                  [ring-reload-modified "0.1.1"]
                  [net.java.dev.jets3t/jets3t "0.8.1"]
                  [org.mindrot/jbcrypt "0.3m"]])
