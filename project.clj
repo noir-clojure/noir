cb1ab76caef8fa3a877525f1eb5bf07e1fbaa394
Chris Granger <ibdknox@gmail.com>
2012-03-20 09:43:06 -0700

    use latest cheshire
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir "1.3.0-beta1"
+(defproject noir "1.3.0-beta2"
   :description "Noir - a clojure web framework"
   :url "http://webnoir.org"
   :codox {:exclude [noir.exception noir.content.defaults
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
                  [org.clojure/tools.namespace "0.1.0"]
-                 [cheshire "2.1.0"]
+                 [cheshire "3.0.0"]
                  [ring "1.0.2"]
                  [hiccup "1.0.0-beta1"]
                  [clj-stacktrace "0.2.4"]
