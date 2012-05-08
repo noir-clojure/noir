c454c5b5c845b541cafec449a4c6795b8b261392
Murphy McMahon <pandeiro@gmail.com>
2012-05-08 12:06:36 -0300

    template body can be more than just vectors, so parse accordingly

--- src/noir/core.clj
+++ src/noir/core.clj
@@ -104,7 +104,8 @@
   "Create a function that returns html using hiccup. The function is callable with the given name. Can optionally include a docstring or metadata map, like a normal function declaration."
   [fname & args]
   (let [[fname params] (macro/name-with-attributes fname args)
-        body (rest (filter vector? args))]
+        body (if-let [fv (first (filter vector? args))]
+               (second (split-at (inc (.indexOf args fv)) args)))]
     `(defn ~fname ~@params
        (html
         ~@body))))
