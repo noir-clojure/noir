979951e2c847330013e5e0fc0649b8ce5ebcb01f
Anthony Grimes <i@raynes.me>
2012-05-07 22:10:51 -0500

    Update hiccup and compojure deps.

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :codox {:exclude [noir.exception noir.content.defaults
                     noir.content.getting-started]}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [compojure "1.0.1"]
+                 [compojure "1.0.4"]
                  [org.clojure/tools.namespace "0.1.0"]
                  [cheshire "4.0.0"]
                  [ring "1.0.2"]
-                 [hiccup "1.0.0-beta1"]
+                 [hiccup "1.0.0"]
                  [clj-stacktrace "0.2.4"]
                  [org.mindrot/jbcrypt "0.3m"]])
