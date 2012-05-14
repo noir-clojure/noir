5e957aff36cbc171167471ae6874af9d9ee2572b
Anthony Grimes <i@raynes.me>
2012-05-14 02:21:31 -0500

    Inc beta.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir "1.3.0-beta6"
+(defproject noir "1.3.0-beta7"
   :description "Noir - a clojure web framework"
   :url "http://webnoir.org"
   :codox {:exclude [noir.exception noir.content.defaults
