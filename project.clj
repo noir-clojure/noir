d18e3150c9d906696b23633ffb29ac280b8da330
Anthony Grimes <i@raynes.me>
2012-06-23 16:46:36 -0500

    Inc lib-noir version and beta.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject noir "1.3.0-beta9"
+(defproject noir "1.3.0-beta10"
   :description "Noir - a clojure web framework"
   :url "http://webnoir.org"
   :codox {:exclude [noir.exception noir.content.defaults
                     noir.content.getting-started]}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lib-noir "0.1.0"]
+                 [lib-noir "0.1.1"]
                  [compojure "1.0.4"]
                  [bultitude "0.1.5"]
                  [ring "1.1.1"]
