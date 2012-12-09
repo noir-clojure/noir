fe3bb7a8e16acdeb5b21879cc14286a2785067a9
Zaiste! <oh@zaiste.net>
2012-12-09 23:18:54 +0100

    dependencies updated

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject noir "1.3.0-beta10"
+(defproject noir "1.4.0-beta1"
   :description "Noir - a clojure web framework"
   :url "http://webnoir.org"
   :codox {:exclude [noir.exception noir.content.defaults
                     noir.content.getting-started]}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [lib-noir "0.1.1"]
-                 [compojure "1.0.4"]
+                 [compojure "1.1.3"]
                  [bultitude "0.1.5"]
-                 [ring "1.1.1"]
+                 [ring "1.1.3"]
                  [hiccup "1.0.0"]
                  [clj-stacktrace "0.2.4"]
                  [org.clojure/tools.macro "0.1.1"]])
