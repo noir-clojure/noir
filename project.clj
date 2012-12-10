db7ddbf6addd0c9e24320e7613fb8a8110e70b8b
Anthony Grimes <i@raynes.me>
2012-12-09 21:01:29 -0600

    Add license info.

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
-(defproject noir "1.3.0-beta10"
+(defproject noir "1.3.0"
   :description "Noir - a clojure web framework"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "http://webnoir.org"
   :codox {:exclude [noir.exception noir.content.defaults
                     noir.content.getting-started]}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lib-noir "0.1.1"]
-                 [compojure "1.0.4"]
-                 [bultitude "0.1.5"]
-                 [ring "1.1.1"]
-                 [hiccup "1.0.0"]
-                 [clj-stacktrace "0.2.4"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [lib-noir "0.2.0"]
+                 [compojure "1.1.3"]
+                 [bultitude "0.2.0"]
+                 [ring "1.1.6"]
+                 [hiccup "1.0.2"]
+                 [clj-stacktrace "0.2.5"]
                  [org.clojure/tools.macro "0.1.1"]])
